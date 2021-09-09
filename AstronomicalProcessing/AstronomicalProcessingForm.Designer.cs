18e45049ec71f0c5610dd5f0e37ad81c13967bda
James Boyd <88956638+jamesramsayboyd@users.noreply.github.com>
2021-09-09 16:25:38 +0800

    Add files via upload
    09/09/2021
    Astronomical Processing Form v1.0
    Code implemented for Fill Array/Search/Sort buttons
    No error trapping
    Add/Remove/Edit buttons non-functional

---
+++ AstronomicalProcessing/AstronomicalProcessingForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace AstronomicalProcessing
+{
+    partial class AstronomicalProcessingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBoxInput = new System.Windows.Forms.TextBox();
+            this.ButtonAdd = new System.Windows.Forms.Button();
+            this.ButtonSort = new System.Windows.Forms.Button();
+            this.ButtonRemove = new System.Windows.Forms.Button();
+            this.ButtonSearch = new System.Windows.Forms.Button();
+            this.ButtonEdit = new System.Windows.Forms.Button();
+            this.ButtonFill = new System.Windows.Forms.Button();
+            this.ListBoxOutput = new System.Windows.Forms.ListBox();
+            this.StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.StripStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ASTRONOMICAL PROCESSING";
+            // 
+            // TextBoxInput
+            // 
+            this.TextBoxInput.Location = new System.Drawing.Point(24, 46);
+            this.TextBoxInput.Name = "TextBoxInput";
+            this.TextBoxInput.Size = new System.Drawing.Size(218, 20);
+            this.TextBoxInput.TabIndex = 1;
+            // 
+            // ButtonAdd
+            // 
+            this.ButtonAdd.Location = new System.Drawing.Point(23, 72);
+            this.ButtonAdd.Name = "ButtonAdd";
+            this.ButtonAdd.Size = new System.Drawing.Size(101, 23);
+            this.ButtonAdd.TabIndex = 2;
+            this.ButtonAdd.Text = "ADD ITEM";
+            this.ButtonAdd.UseVisualStyleBackColor = true;
+            this.ButtonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // ButtonSort
+            // 
+            this.ButtonSort.Location = new System.Drawing.Point(145, 72);
+            this.ButtonSort.Name = "ButtonSort";
+            this.ButtonSort.Size = new System.Drawing.Size(97, 23);
+            this.ButtonSort.TabIndex = 3;
+            this.ButtonSort.Text = "SORT";
+            this.toolTip1.SetToolTip(this.ButtonSort, "Sort data in ascending order");
+            this.ButtonSort.UseVisualStyleBackColor = true;
+            this.ButtonSort.Click += new System.EventHandler(this.ButtonSort_Click);
+            // 
+            // ButtonRemove
+            // 
+            this.ButtonRemove.Location = new System.Drawing.Point(23, 102);
+            this.ButtonRemove.Name = "ButtonRemove";
+            this.ButtonRemove.Size = new System.Drawing.Size(101, 23);
+            this.ButtonRemove.TabIndex = 4;
+            this.ButtonRemove.Text = "REMOVE ITEM";
+            this.ButtonRemove.UseVisualStyleBackColor = true;
+            this.ButtonRemove.Click += new System.EventHandler(this.ButtonRemove_Click);
+            // 
+            // ButtonSearch
+            // 
+            this.ButtonSearch.Location = new System.Drawing.Point(145, 102);
+            this.ButtonSearch.Name = "ButtonSearch";
+            this.ButtonSearch.Size = new System.Drawing.Size(97, 23);
+            this.ButtonSearch.TabIndex = 5;
+            this.ButtonSearch.Text = "SEARCH";
+            this.toolTip1.SetToolTip(this.ButtonSearch, "Enter a number in the Text Box to search");
+            this.ButtonSearch.UseVisualStyleBackColor = true;
+            this.ButtonSearch.Click += new System.EventHandler(this.ButtonSearch_Click);
+            // 
+            // ButtonEdit
+            // 
+            this.ButtonEdit.Location = new System.Drawing.Point(23, 132);
+            this.ButtonEdit.Name = "ButtonEdit";
+            this.ButtonEdit.Size = new System.Drawing.Size(101, 23);
+            this.ButtonEdit.TabIndex = 6;
+            this.ButtonEdit.Text = "EDIT ITEM";
+            this.ButtonEdit.UseVisualStyleBackColor = true;
+            this.ButtonEdit.Click += new System.EventHandler(this.ButtonEdit_Click);
+            // 
+            // ButtonFill
+            // 
+            this.ButtonFill.Location = new System.Drawing.Point(145, 132);
+            this.ButtonFill.Name = "ButtonFill";
+            this.ButtonFill.Size = new System.Drawing.Size(97, 23);
+            this.ButtonFill.TabIndex = 7;
+            this.ButtonFill.Text = "FILL ARRAY";
+            this.toolTip1.SetToolTip(this.ButtonFill, "Click to populate array with randomly generated numbers");
+            this.ButtonFill.UseVisualStyleBackColor = true;
+            this.ButtonFill.Click += new System.EventHandler(this.ButtonFill_Click);
+            // 
+            // ListBoxOutput
+            // 
+            this.ListBoxOutput.FormattingEnabled = true;
+            this.ListBoxOutput.Location = new System.Drawing.Point(24, 161);
+            this.ListBoxOutput.Name = "ListBoxOutput";
+            this.ListBoxOutput.Size = new System.Drawing.Size(218, 316);
+            this.ListBoxOutput.TabIndex = 8;
+            // 
+            // StatusStrip
+            // 
+            this.StatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StripStatus});
+            this.StatusStrip.Location = new System.Drawing.Point(0, 494);
+            this.StatusStrip.Name = "StatusStrip";
+            this.StatusStrip.Size = new System.Drawing.Size(271, 22);
+            this.StatusStrip.TabIndex = 9;
+            this.StatusStrip.Text = "-";
+            // 
+            // StripStatus
+            // 
+            this.StripStatus.Name = "StripStatus";
+            this.StripStatus.Size = new System.Drawing.Size(0, 17);
+            // 
+            // AstronomicalProcessingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 516);
+            this.Controls.Add(this.StatusStrip);
+            this.Controls.Add(this.ListBoxOutput);
+            this.Controls.Add(this.ButtonFill);
+            this.Controls.Add(this.ButtonEdit);
+            this.Controls.Add(this.ButtonSearch);
+            this.Controls.Add(this.ButtonRemove);
+            this.Controls.Add(this.ButtonSort);
+            this.Controls.Add(this.ButtonAdd);
+            this.Controls.Add(this.TextBoxInput);
+            this.Controls.Add(this.label1);
+            this.Name = "AstronomicalProcessingForm";
+            this.Text = "Astronomical Processing";
+            this.toolTip1.SetToolTip(this, "Click to populate array with randomly generated numbers");
+            this.StatusStrip.ResumeLayout(false);
+            this.StatusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBoxInput;
+        private System.Windows.Forms.Button ButtonAdd;
+        private System.Windows.Forms.Button ButtonSort;
+        private System.Windows.Forms.Button ButtonRemove;
+        private System.Windows.Forms.Button ButtonSearch;
+        private System.Windows.Forms.Button ButtonEdit;
+        private System.Windows.Forms.Button ButtonFill;
+        private System.Windows.Forms.ListBox ListBoxOutput;
+        private System.Windows.Forms.StatusStrip StatusStrip;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolStripStatusLabel StripStatus;
+    }
+}
+
---
+++ AstronomicalProcessing/AstronomicalProcessingForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// C Sharp 1 Assessment 2
+// James Boyd, Team Elljam, Sprint Number One
+// Date: 09/09/2021
+// Version 1.0 (Fill, Search and Sort functions implemented)
+// Astronomical Processing
+//
+// Explanation: A program for a local observatory that stores the number of neutrino
+// interactions per hour as integer values in an array of size 24. This forms-based 
+// GUI application displays the data in a list box and uses text boxes and buttons for processing.
+//
+// Functionality: User can add/remove/edit data using a textbox for input. Data can be
+// sorted or searched. An option to randomly generate data with a button press is included
+
+
+namespace AstronomicalProcessing
+{
+    public partial class AstronomicalProcessingForm : Form
+    {
+        public AstronomicalProcessingForm()
+        {
+            InitializeComponent();
+        }
+        // Creating array to hold 24 integers
+        static int max = 24;
+        int[] astroArray = new int[max];
+        Random rnd = new Random();
+
+        #region Add/Remove/Edit
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            // TODO: Enable user to add item to the array
+            // Add error trapping to ensure user enters an integer
+            // Status strip message confirming item added/telling user to type valid integer
+            // Button tooltip: "Enter an item to add it to the list"
+        }
+
+        private void ButtonRemove_Click(object sender, EventArgs e)
+        {
+            // TODO: Enable user to click an item in Listbox or enter item in Textbox
+            // Ask user for confirmation with DialogResult delRecord MessageBox code
+            // Remove that item from the array
+            // Add error trapping to ensure user enters integer
+            // Status strip message confirming item deletion/telling user to enter valid item
+            // Button tooltip: "Select an item to delete"
+        }
+
+        private void ButtonEdit_Click(object sender, EventArgs e)
+        {
+            // TODO: Enable user to click an item in Listbox
+            // User will enter new number in textbox, click Edit button, item will be updated
+            // DialogResult confirmation required before editing
+            // Add error trapping to ensure user has clicked on an item
+            // Button tooltip: "Select an item to edit"
+
+        }
+        #endregion
+
+        #region Sort/Search
+        // Calls Bubble Sort algorithm to sort array and display in Listbox
+        private void ButtonSort_Click(object sender, EventArgs e)
+        {
+            SortArray();
+            DisplayArray();
+        }
+
+        // Bubble Sort algorithm
+        private void SortArray()
+        {
+            // TODO: Add error trapping (make sure list isn't empty before Sort is clicked)
+            for (int i = 0; i < max; i++)
+            {
+                for (int j = i + 1; j < max; j++)
+                {
+                    if (astroArray[i] > astroArray[j])
+                    {
+                        int temp = astroArray[i];
+                        astroArray[i] = astroArray[j];
+                        astroArray[j] = temp;
+                    }
+                }
+            }
+        }
+
+        // Searches for a target using Binary Search algorithm
+        private void ButtonSearch_Click(object sender, EventArgs e)
+        {
+            // TODO: Add error trapping (make sure user can't enter string/double/etc)
+            // TODO: Add useful error/feedback messages in status strip
+            
+            SortArray();
+            DisplayArray();
+            Int32.TryParse(TextBoxInput.Text, out int target);
+            int lowBound = 0;
+            int highBound = max - 1;
+            int mid = 0;
+            bool found = false;
+
+            while (lowBound <= highBound)
+            {
+                mid = (lowBound + highBound) / 2;
+                if (target == astroArray[mid])
+                {
+                    found = true;
+                    break;
+                }
+                else if (target > astroArray[mid])
+                {
+                    lowBound = mid + 1;
+                }
+                else
+                {
+                    highBound = mid - 1;
+                }
+            }
+
+            if(found)
+            {
+                // clear textbox
+                // highlight search target in listbox
+                // display "found at index __" in status strip
+                StripStatus.Text = "Found";
+            }
+            else
+            {
+                // clear textbox
+                // display "item not found" in status strip
+            }
+        }
+        #endregion
+
+        #region Utilities
+        // Fills astroArray with 24 randomly generated integers between 10 and 99
+        private void ButtonFill_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < max; x++)
+            {
+                astroArray[x] = rnd.Next(10, 99);
+            }
+            DisplayArray();
+        }
+
+        // Displays array in Listbox
+        private void DisplayArray()
+        {
+            ListBoxOutput.Items.Clear();
+            for (int y = 0; y < max; y++)
+            {
+                ListBoxOutput.Items.Add(astroArray[y]);
+            }
+        }
+        #endregion
+    }
+}
